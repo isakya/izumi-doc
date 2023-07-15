e5043327fa926cfe18f73b5e1ecf8b54f3e8d928
isakya <1016361268@qq.com>
2023-07-16 00:18:03 +0800

    制作CopyUtil封装BeanUtils

--- src/main/java/com/izumi/wiki/service/EbookService.java
+++ src/main/java/com/izumi/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.izumi.wiki.domain.EbookExample;
 import com.izumi.wiki.mapper.EbookMapper;
 import com.izumi.wiki.req.EbookReq;
 import com.izumi.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.izumi.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +23,9 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
 
-        return respList;
+        return list;
     }
 }
