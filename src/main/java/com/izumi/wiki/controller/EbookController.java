af3faa486d811835c7888914831e32e984d5ca46
isakya <1016361268@qq.com>
2023-07-15 23:49:32 +0800

    封装请求参数和返回参数

--- src/main/java/com/izumi/wiki/controller/EbookController.java
+++ src/main/java/com/izumi/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.izumi.wiki.controller;
 
-import com.izumi.wiki.domain.Ebook;
+import com.izumi.wiki.req.EbookReq;
 import com.izumi.wiki.resp.CommonResp;
+import com.izumi.wiki.resp.EbookResp;
 import com.izumi.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,9 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
