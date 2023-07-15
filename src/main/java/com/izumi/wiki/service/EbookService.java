94789c823aeac9afd114cc8f257caf772f74e3d7
isakya <1016361268@qq.com>
2023-07-16 07:07:45 +0800

    集成PageHelper插件 将mapper日志级别改为trace

--- src/main/java/com/izumi/wiki/service/EbookService.java
+++ src/main/java/com/izumi/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.izumi.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.izumi.wiki.domain.Ebook;
 import com.izumi.wiki.domain.EbookExample;
 import com.izumi.wiki.mapper.EbookMapper;
 import com.izumi.wiki.req.EbookReq;
 import com.izumi.wiki.resp.EbookResp;
 import com.izumi.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,7 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -25,7 +30,12 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3); // 只对第一次遇到的sql查询有效
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
 
         // 列表复制
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
