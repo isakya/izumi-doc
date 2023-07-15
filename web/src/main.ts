4e32fb123d19c0d23d1619a781bba28a6b5af0e4
isakya <1016361268@qq.com>
2023-07-16 04:28:26 +0800

    修改axios的baseUrl读配置文件

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import axios from "axios";
+axios.defaults.baseURL = process.env.VUE_APP_SERVER
 
 createApp(App).use(store).use(router).use(Antd).mount('#app')
 
