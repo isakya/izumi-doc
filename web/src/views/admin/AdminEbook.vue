b150597b0834e316697477394400507e73cd84b8
isakya <1016361268@qq.com>
2023-07-16 06:46:45 +0800

    电子书管理表格展示数据

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -1,9 +1,130 @@
 <template>
   <a-layout style="padding: 24px 0; background: #fff; margin-top: 50px">
     <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-      <div class="ebook">
-        <h1>电子书管理</h1>
-      </div>
+      <a-table
+          :columns="columns"
+          :data-source="ebooks"
+          :row-key="record => record.id"
+          :pagination="pagination"
+          :loading="loading"
+          @change="handleTableChange"
+      >
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.key === 'cover'">
+            <img :src="record.cover" alt="avatar"/>
+          </template>
+          <template v-if="column.key === 'action'">
+            <a-space size="small">
+              <a-button type="primary">
+                编辑
+              </a-button>
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-space>
+            </template>
+          </template>
+      </a-table>
     </a-layout-content>
   </a-layout>
 </template>
+<script lang="ts" setup>
+import {onMounted, ref} from 'vue'
+import axios from 'axios'
+import {message} from 'ant-design-vue'
+
+const ebooks = ref()
+const pagination = ref({
+  current: 1,
+  pageSize: 2,
+  total: 0
+})
+const loading = ref(false)
+
+const columns = [
+  {
+    title: '封面',
+    dataIndex: 'cover',
+    key: 'cover'
+  },
+  {
+    title: '名称',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: '分类一',
+    dataIndex: 'category1Id',
+    key: 'category1Id',
+  },
+  {
+    title: '分类二',
+    dataIndex: 'category2Id',
+    key: 'category2Id'
+  },
+  {
+    title: '文档数',
+    dataIndex: 'docCount',
+    key: 'docCount'
+  },
+  {
+    title: '阅读数',
+    dataIndex: 'viewCount',
+    key: 'viewCount',
+  },
+  {
+    title: '点赞数',
+    dataIndex: 'voteCount',
+    key: 'voteCount',
+
+  },
+  {
+    title: 'Action',
+    key: 'action'
+  }
+]
+
+/**
+ * 数据查询
+ */
+const handleQuery = (params: any) => {
+  loading.value = true
+  // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑
+  ebooks.value = []
+  axios.get("/ebook/list", {
+    params: {
+      page: params.page,
+      size: params.size,
+      name: params.value?.name
+    }
+  }).then((response) => {
+    loading.value = false
+    const data = response.data
+    if (data.success) {
+      ebooks.value = data.content
+      pagination.value.current = params.page
+      pagination.value.total = data.content.total
+    } else {
+      message.error(data.message)
+    }
+  })
+}
+// 表格点击页码时触发
+const handleTableChange = (pagination: any) => {
+  console.log('看看自带的分页参数都有啥: ' + pagination)
+  handleQuery({
+    page: pagination.current,
+    size: pagination.pageSize
+  })
+}
+onMounted(() => {
+  handleQuery({})
+})
+</script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
