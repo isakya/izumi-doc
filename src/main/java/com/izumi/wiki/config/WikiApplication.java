6860ec4445aa4f29de7b4db073f0847d86168045
isakya <1016361268@qq.com>
2023-07-15 17:35:02 +0800

    开发hello world接口

--- src/main/java/com/izumi/wiki/config/WikiApplication.java
+++ src/main/java/com/izumi/wiki/config/WikiApplication.java
@@ -1,11 +1,15 @@
-package com.izumi.wiki;
+package com.izumi.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+@ComponentScan({"com.izumi", "com.test"}) // 扫描多个包
+@ComponentScan("com.izumi")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
