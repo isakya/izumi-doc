45b1828bb3708eb36296a239e674520767e3c654
isakya <1016361268@qq.com>
2023-07-15 19:54:59 +0800

    集成mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,13 @@ create table test(
                      password varchar(50) comment '密码',
                      primary key(id)
 )engine=innodb default charset=utf8mb4 comment='测试';
+
+
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`   bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment '测试代码生成器';
