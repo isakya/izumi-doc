fb23dd1afbc24c6e3e26500d194693f6220c76a1
isakya <1016361268@qq.com>
2023-07-16 05:50:56 +0800

    增加电子书管理页面 新页面开发：页面、路由、菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import AdminEbook from '../views/admin/AdminEbook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,7 +17,12 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
+  },
 ]
 
 const router = createRouter({
---
+++ web/src/views/admin/AdminEbook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="ebook">
+    <h1>电子书管理</h1>
+  </div>
+</template>
