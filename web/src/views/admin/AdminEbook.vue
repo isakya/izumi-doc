389ade40a1c92c8aa3b404365c35578289a783ad
isakya <1016361268@qq.com>
2023-07-16 05:56:19 +0800

    修改电子书管理页面的布局

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="ebook">
-    <h1>电子书管理</h1>
-  </div>
+  <a-layout style="padding: 24px 0; background: #fff; margin-top: 50px">
+    <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+      <div class="ebook">
+        <h1>电子书管理</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
