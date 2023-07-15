537890773d2e0508663cf3601e52126d48472079
isakya <1016361268@qq.com>
2023-07-15 20:10:25 +0800

    完成电子书列表查询接口

---
+++ src/main/java/com/izumi/wiki/controller/EbookController.java
@@ -0,0 +1,26 @@
+package com.izumi.wiki.controller;
+
+import com.izumi.wiki.domain.Ebook;
+import com.izumi.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+
+    @Resource
+    private EbookService ebookService;
+
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/izumi/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.izumi.wiki.service;
+
+import com.izumi.wiki.domain.Ebook;
+import com.izumi.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
