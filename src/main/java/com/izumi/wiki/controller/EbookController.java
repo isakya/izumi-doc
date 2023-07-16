85988f55d4a4f9f4df91ca74fd43943e3938da9d
isakya <1016361268@qq.com>
2023-07-16 17:26:09 +0800

    封装分页请求参数PageReq和返回参数PageResp

--- src/main/java/com/izumi/wiki/controller/EbookController.java
+++ src/main/java/com/izumi/wiki/controller/EbookController.java
@@ -3,13 +3,13 @@ package com.izumi.wiki.controller;
 import com.izumi.wiki.req.EbookReq;
 import com.izumi.wiki.resp.CommonResp;
 import com.izumi.wiki.resp.EbookResp;
+import com.izumi.wiki.resp.PageResp;
 import com.izumi.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 
 @RestController
@@ -23,8 +23,8 @@ public class EbookController {
 
     @GetMapping("/list")
     public CommonResp list(EbookReq req) {
-        CommonResp<List<EbookResp>> resp = new CommonResp<>();
-        List<EbookResp> list = ebookService.list(req);
+        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
+        PageResp<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/izumi/wiki/service/EbookService.java
+++ src/main/java/com/izumi/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.izumi.wiki.domain.EbookExample;
 import com.izumi.wiki.mapper.EbookMapper;
 import com.izumi.wiki.req.EbookReq;
 import com.izumi.wiki.resp.EbookResp;
+import com.izumi.wiki.resp.PageResp;
 import com.izumi.wiki.util.CopyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,23 +24,26 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list(EbookReq req) {
+    public PageResp<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
 
         if(!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
-        PageHelper.startPage(1, 3); // 只对第一次遇到的sql查询有效
+        PageHelper.startPage(req.getPage(), req.getSize()); // 只对第一次遇到的sql查询有效
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
         LOG.info("总行数：{}",pageInfo.getTotal());
         LOG.info("总页数：{}",pageInfo.getPages());
 
+
         // 列表复制
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
-
-        return list;
+        PageResp<EbookResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
     }
 }
