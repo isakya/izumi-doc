c3721281a68ead123c306552c63fa56a0ecac80c
isakya <1016361268@qq.com>
2023-07-16 17:48:37 +0800

    首页显示电子书列表，使用固定分页参数，一次性查出所有的电子书

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -83,9 +83,14 @@ const actions: Record<string, any>[] = [
 const ebooks = ref()
 
 onMounted(() => {
-  axios.get("/ebook/list").then(res => {
-    const data = res.data;
-    ebooks.value = data.content
+  axios.get("/ebook/list", {
+    params: {
+      page: 1,
+      size: 1000
+    }
+  }).then(res => {
+    const data = res.data
+    ebooks.value = data.content.list
     console.log(res)
   })
 })
