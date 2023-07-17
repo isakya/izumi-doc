94a734d7259e7fe80f6b9f3e26ecd0abc46f38a0
isakya <1016361268@qq.com>
2023-07-17 18:54:09 +0800

    使用递归算法，将树形结构数据中，当前目标节点及其子孙节点设置为disabled; 解决编辑保存后，重新编辑时，数据不更新时的问题; 为树形选择增加一个"无"的选择，作为一级节点;

--- web/src/views/admin/AdminCategory.vue
+++ web/src/views/admin/AdminCategory.vue
@@ -147,6 +147,7 @@ const handleQuery = () => {
   loading.value = true
   // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑
   categorys.value = []
+  level1.value = []
   axios.get("/category/all").then((response) => {
     loading.value = false
     const data = response.data
--- web/src/views/admin/AdminDoc.vue
+++ web/src/views/admin/AdminDoc.vue
@@ -53,14 +53,16 @@
         <a-input v-model:value="doc.name"/>
       </a-form-item>
       <a-form-item label="父文档">
-        <a-select
+        <a-tree-select
             v-model:value="doc.parent"
-            ref="select">
-          <a-select-option value="0">无</a-select-option>
-          <a-select-option v-for="item in level1" :key="item.id" :value="item.id" :disabled="doc.id === item.id">
-            {{ item.name }}
-          </a-select-option>
-        </a-select>
+            style="width: 100%"
+            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+            placeholder="请选择父文档"
+            tree-default-expand-all
+            :tree-data="treeSelectData"
+            :replaceFields="{label: 'name', key: 'id', value: 'id'}"
+        >
+        </a-tree-select>
       </a-form-item>
       <a-form-item label="排序">
         <a-input v-model:value="doc.sort"/>
@@ -86,11 +88,21 @@ const modalLoading = ref<boolean>(false)
 const edit = (record: any) => {
   modalVisible.value = true
   doc.value = Tool.copy(record)
+
+  // 不能选择当前节点及其所有子孙节点
+  treeSelectData.value = Tool.copy(level1.value)
+  setDisable(treeSelectData.value, record.id)
+  // 为选择树添加一个 "无"字
+  treeSelectData.value.unshift({id: 0, name: '无'})
 };
 // 新增
 const add = () => {
   modalVisible.value = true
   doc.value = {}
+
+  treeSelectData.value = Tool.copy(level1.value)
+  // 为选择树添加一个 "无"字
+  treeSelectData.value.unshift({id: 0, name: '无'})
 }
 // 删除
 const del = (id: number) => {
@@ -139,6 +151,37 @@ const columns = [
     key: 'action'
   }
 ]
+// 树选择组件会随着当前编辑的节点而变化，所以单独声明一个响应式的变量
+const treeSelectData = ref()
+treeSelectData.value = []
+
+// 将某节点及其子子孙孙节点全部置为disabled
+const setDisable = (treeSelectData: any, id: any) => {
+  console.log(treeSelectData, id)
+  // 遍历数组，即遍历某一层节点
+  for (let i = 0; i < treeSelectData.length; i++) {
+    const node = treeSelectData[i]
+    if (node.id === id) {
+      // 如果当前节点就是目标节点
+      console.log("disabled", node)
+      // 将目标节点设置为disabled
+      node.disabled = true
+      // 遍历所有自及诶单，将所有子节点全部都加上disabled
+      const children = node.children
+      if (Tool.isNotEmpty(children)) {
+        for (let j = 0; j < children.length; j++) {
+          setDisable(children, children[j].id)
+        }
+      }
+    } else {
+      // 如果当前节点不是目标节点，则到其子节点再找找看
+      const children = node.children
+      if (Tool.isNotEmpty(children)) {
+        setDisable(children, id)
+      }
+    }
+  }
+}
 
 /**
  * 数据查询
@@ -147,6 +190,8 @@ const handleQuery = () => {
   loading.value = true
   // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑
   docs.value = []
+  // 清除表格数据
+  level1.value = []
   axios.get("/doc/all").then((response) => {
     loading.value = false
     const data = response.data
