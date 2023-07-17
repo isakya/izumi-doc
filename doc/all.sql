b443d2f4b23ede5e8189736255787dba718757d3
isakya <1016361268@qq.com>
2023-07-18 00:18:47 +0800

    文档内容表的设计和代码生成

--- doc/all.sql
+++ doc/all.sql
@@ -95,3 +95,12 @@ values (1, 1, 0, '文档1', 1, 0, 0),
        (4, 1, 3, '文档2.1', 1, 0, 0),
        (5, 1, 3, '文档2.2', 2, 0, 0),
        (6, 1, 5, '文档2.2.1', 1, 0, 0);
+
+# 文档内容
+drop table if exists `content`;
+create table `content`
+(
+    `id`      bigint     not null comment '文档id',
+    `content` mediumtext not null comment '内容',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='文档内容';
