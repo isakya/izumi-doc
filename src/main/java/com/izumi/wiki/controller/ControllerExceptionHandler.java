9fe49bf80c963edd16b72a16de132db6679dad8b
isakya <1016361268@qq.com>
2023-07-20 04:52:49 +0800

    增加校验用户名不能重复，增加自定义业务异常

--- src/main/java/com/izumi/wiki/controller/ControllerExceptionHandler.java
+++ src/main/java/com/izumi/wiki/controller/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.izumi.wiki.controller;
 
 
+import com.izumi.wiki.exception.BusinessException;
 import com.izumi.wiki.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +30,36 @@ public class ControllerExceptionHandler {
         LOG.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+
+    /**
+     * 统一校验业务异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(BusinessException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.warn("业务异常: {}", e.getCode().getDesc());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getCode().getDesc());
+        return commonResp;
+    }
+
+    /**
+     * 统一校验系统异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常: ", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常，请联系管理员");
         return commonResp;
     }
 }
--- src/main/java/com/izumi/wiki/service/UserService.java
+++ src/main/java/com/izumi/wiki/service/UserService.java
@@ -4,16 +4,19 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.izumi.wiki.domain.User;
 import com.izumi.wiki.domain.UserExample;
+import com.izumi.wiki.exception.BusinessException;
+import com.izumi.wiki.exception.BusinessExceptionCode;
 import com.izumi.wiki.mapper.UserMapper;
 import com.izumi.wiki.req.UserQueryReq;
 import com.izumi.wiki.req.UserSaveReq;
-import com.izumi.wiki.resp.UserQueryResp;
 import com.izumi.wiki.resp.PageResp;
+import com.izumi.wiki.resp.UserQueryResp;
 import com.izumi.wiki.util.CopyUtil;
 import com.izumi.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -61,9 +64,15 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 新增
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            if(ObjectUtils.isEmpty(selectByLoginName(req.getLoginName()))) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                // 用户名已存在
+                // 提示用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         } else {
             // 更新
             userMapper.updateByPrimaryKey(user);
@@ -77,5 +86,18 @@ public class UserService {
      */
     public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(LoginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if(CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 }
