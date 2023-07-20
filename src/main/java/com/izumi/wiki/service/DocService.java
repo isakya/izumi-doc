3585388f87f1e15d721c4fbe8df8cf7b3bb5032a
isakya <1016361268@qq.com>
2023-07-21 04:21:33 +0800

    加入事务注解 事务注解了的方法不能在同一个类中调用，否则事务会失效

--- src/main/java/com/izumi/wiki/service/DocService.java
+++ src/main/java/com/izumi/wiki/service/DocService.java
@@ -22,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -90,6 +91,7 @@ public class DocService {
      *
      * @param req
      */
+    @Transactional
     public void save(DocSaveReq req) {
         Doc doc = CopyUtil.copy(req, Doc.class);
         Content content = CopyUtil.copy(req, Content.class);
