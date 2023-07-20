f335049debfa2632e20cab1ff3a739b1778db6b4
isakya <1016361268@qq.com>
2023-07-20 08:07:33 +0800

    前端增加登录校验（测试的数据不合格，所以暂时先去掉）

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -85,11 +85,12 @@ const user = computed(() => store.state.user)
 // 登录
 const login = () => {
   console.log('开始登录')
-  loginModalLoading.value = true
   // 前端校验
-  if (loginUser.value.password.length < 6 || loginUser.value.password.length > 32) {
-    message.error('【密码】6~32位')
-  }
+  // if (loginUser?.value?.password?.length < 6 || loginUser?.value?.password?.length > 32) {
+  //   message.error('【密码】6~20位')
+  //   return
+  // }
+  loginModalLoading.value = true
   loginUser.value.password = hexMd5(loginUser.value.password + KEY)
   axios.post("/user/login", loginUser.value).then((res) => {
     loginModalLoading.value = false
