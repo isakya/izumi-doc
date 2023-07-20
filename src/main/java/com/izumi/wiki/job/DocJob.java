36a9ff18334ff0bd485ff0fe82287aa4d77cec83
isakya <1016361268@qq.com>
2023-07-21 03:34:10 +0800

    增加日志流水号，方便生产运维

--- src/main/java/com/izumi/wiki/job/DocJob.java
+++ src/main/java/com/izumi/wiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.izumi.wiki.job;
 
 import com.izumi.wiki.service.DocService;
+import com.izumi.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +18,9 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 每30秒更新电子书信息
      */
@@ -25,6 +30,9 @@ public class DocJob {
 //        String dateString = format.format(new Date());
 //        Thread.sleep(1500);
 //        LOG.info("每隔1.5秒执种执行一次: {}", dateString);
+
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("更新电子书下的文档数据开始");
         long startTime = System.currentTimeMillis();
         docService.updateEbookInfo();
